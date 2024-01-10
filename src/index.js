eaa9a338b08703afc7f1475e5361e10558b6ec10
SHAFIN SHAIKH <shafinshaikh25@gmail.com>
2024-01-10 14:39:15 +0530

    final working code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import KeyInput from './KeyInput.js';
-import connect from './Connect.js';
+import { connect, getBuildings } from './Connect.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -22,12 +22,13 @@ const ground = new THREE.Mesh( geometry, material );
 scene.add(ground);
 camera.position.set( 5, 15, 15 );
 
-const boxGeometry = new THREE.BoxGeometry( 2, 2, 2 );
-const boxMaterial = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
-const box = new THREE.Mesh( boxGeometry, boxMaterial );
-box.position.set(-2,0,8);
+// const boxGeometry = new THREE.BoxGeometry( 2, 2, 2 );
+// const boxMaterial = new THREE.MeshPhongMaterial( { color: 0x00ff00 } );
+// const box = new THREE.Mesh( boxGeometry, boxMaterial );
+// box.position.set(-2,0,8);
 
-scene.add(box);
+// scene.add(box);
+
 
 
 function animate() {
@@ -46,6 +47,25 @@ function animate() {
 }
 animate();
 
-connect.then(() => {
-	console.log("test print for connect function call in index.js");
+// Function to create a 3D building
+function createBuilding(building) {
+    const w = parseFloat(building.w); // Convert to number
+    const h = parseFloat(building.h); // Convert to number
+    const d = parseFloat(building.d); // Convert to number
+    const x = parseFloat(building.x); // Convert to number
+    const y = parseFloat(building.y); // Convert to number
+    const z = parseFloat(building.z); // Convert to number
+
+    const geometry = new THREE.BoxGeometry(w, h, d);
+    const material = new THREE.MeshPhongMaterial({ color: 0x0000FF });
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    scene.add(mesh);
+}
+connect.then(async () => {
+    const buildings = await getBuildings();
+    buildings.forEach(createBuilding);
+	console.log(buildings);
+}).catch(error => {
+    console.error('Failed to connect:', error);
 });
